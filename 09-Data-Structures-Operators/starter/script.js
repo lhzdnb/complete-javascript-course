632b41086ddd67b813a5b3d72c904322a99e1d95
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-26 00:10:39 -0800

    Optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,19 +1,21 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 const openingHours = {
-  thu: {
+  [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  fri: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
-  sat: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -21,17 +23,17 @@ const openingHours = {
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
   openingHours,
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.starterMenu[mainIndex]];
   },
 
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
     console.log(starterIndex, mainIndex, time, address);
   },
 
@@ -48,6 +50,30 @@ const restaurant = {
 };
 
 /*
+ //////////////////////////////////////////////
+ // Optional Chaining
+ // if (restaurant.openingHours.mon.open) {
+ //   console.log(restaurant.openingHours.mon.open);
+ // }
+ // With optional chaining
+ console.log(restaurant.openingHours.mon?.open);
+ console.log(restaurant.openingHours?.mon?.open);
+ // Example
+ const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+ for (const day of days) {
+ const open = restaurant.openingHours[day]?.open ?? `closed`;
+ console.log(`On ${day}, we open at ${open}`);
+ }
+ 
+ // Methods
+ console.log(restaurant.order?.(0, 1) ?? "Method does not exit");
+ console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exit");
+ 
+ // Arrays
+ const users = [{ name: "Jonus", email: "hello@jonas.io" }];
+ console.log(users[0]?.name ?? "User array empty");
+ 
+ 
  ///////////////////////////////
  // for of loop
  const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
