60c1012e7dc7fe1857d71e5ac1d1e362fd9133b2
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-25 23:34:09 -0800

    for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,21 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,28 +26,13 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  openingHours,
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.starterMenu[mainIndex]];
   },
 
   orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(starterIndex, mainIndex, time, address);
-  },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
   },
 
   orderPasta(ing1, ing2, ing3) {
@@ -47,31 +47,51 @@ const restaurant = {
   },
 };
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20,
-  numGuests: 0,
-};
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
-
-// rest1.numberGuests = rest1.numGuests || 10;
-// rest2.numberGuests = rest2.numGuests || 10;
-
-// nullish assignment operator (null or undefined)
-rest1.numGuests ??= 10;
-// OR assignment operator
-rest2.numberGuests ||= 10;
-// AND assignment operator
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
-
-console.log(rest1);
-console.log(rest2);
-
 /*
+ ///////////////////////////////
+ // for of loop
+ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+ for (const item of menu) {
+ console.log(item);
+ }
+ 
+ for (const item of menu.entries()) {
+ console.log(item);
+ }
+ 
+ for (const [index, element] of menu.entries()) {
+ console.log(`${index}: ${element}`);
+ }
+ 
+ 
+ 
+ //////////////////////////////////
+ // Assignment Operator
+ const rest1 = {
+ name: 'Capri',
+ // numGuests: 20,
+ numGuests: 0,
+ };
+ const rest2 = {
+ name: 'La Piazza',
+ owner: 'Giovanni Rossi',
+ };
+ 
+ // rest1.numberGuests = rest1.numGuests || 10;
+ // rest2.numberGuests = rest2.numGuests || 10;
+ 
+ // nullish assignment operator (null or undefined)
+ rest1.numGuests ??= 10;
+ // OR assignment operator
+ rest2.numberGuests ||= 10;
+ // AND assignment operator
+ rest1.owner &&= '<ANONYMOUS>';
+ rest2.owner &&= '<ANONYMOUS>';
+ 
+ console.log(rest1);
+ console.log(rest2);
+ 
+ 
  ///////////////////////////////////////////////
  The Nullish Coalescing Operator
  restaurant.numGuests = 0;
