915d2eae335bef248486894247bbd0e06ac596c0
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-01 23:20:17 -0800

    Value vs. Preference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,27 +1,60 @@
 "use strict";
 
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers,
-) {
-  // ES5
-  // numPassengers = numPassengers ?? 1;
-  // price = price ?? 199
+/*
+ const bookings = [];
+ const createBooking = function (
+ flightNum,
+ numPassengers = 1,
+ price = 199 * numPassengers,
+ ) {
+ // ES5
+ // numPassengers = numPassengers ?? 1;
+ // price = price ?? 199
+ 
+ const booking = {
+ flightNum,
+ numPassengers,
+ price,
+ };
+ console.log(booking);
+ bookings.push(booking);
+ };
+ 
+ createBooking("LH123");
+ createBooking("LH123", 2, 800);
+ createBooking("LH123", 2);
+ createBooking("LH123", 5);
+ 
+ createBooking("LH123", undefined, 1000);
+ */
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+const flight = "LH234";
+const lhzdnb = {
+  name: "Hao Liang",
+  passport: 123345678,
 };
 
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
+function checkIn(flightNum, passenger) {
+  flightNum = "lH999";
+  // Reference!!! It is the same memory in the heap
+  passenger.name = "MR. " + passenger.name;
 
-createBooking("LH123", undefined, 1000);
+  if (passenger.passport === 123345678) {
+    alert("Check in");
+  } else {
+    alert("Wrong passport");
+  }
+}
+
+checkIn(flight, lhzdnb);
+console.log(flight);
+console.log(lhzdnb);
+
+function newPassport(person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+}
+
+newPassport(lhzdnb);
+checkIn(flight, lhzdnb);
+console.log(flight);
+console.log(lhzdnb);
