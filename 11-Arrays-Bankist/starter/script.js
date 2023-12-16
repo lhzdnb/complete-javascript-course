0dc47520ada7bed117e00b8222a232e799109f24
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-16 13:21:17 -0800

    code challenge # 2

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -279,3 +279,12 @@ createUsernames(accounts);
 
 /////////////////////////////////////////////////
 // code challenge # 2
+// function calcAverageHumanAge(ages) {
+//   return ages
+//     .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter((age) => age >= 18)
+//     .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+// }
+//
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
