3754e03b7ee4677edd6ddd6c5cdc2f785c172ab7
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-16 00:00:38 -0800

    simple array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,35 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// slice
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2)); // starting from the first element and excluding
+// the last two
+console.log(arr.slice());
+console.log([...arr]);
+
+// splice MUTATE THE ORIGINAL ARRAY
+// console.log(arr.splice(2)); // ['c', 'd', 'e']
+arr.splice(-1);
+arr.splice(1, 2);
+console.log(arr); // ['a', 'b']
+
+// Reverse MUTATE THE ORIGINAL ARRAY
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'j'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// join
+console.log(letters.join(' - '));
