d5d1ba58c9173902d14189c8fefb51bdac93b47b
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-16 02:04:27 -0800

    The map method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -81,7 +81,7 @@ function displayMovement(movements) {
 
 displayMovement(account1.movements);
 
-console.log(containerMovements.innerHTML);
+// console.log(containerMovements.innerHTML);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -186,7 +186,43 @@ console.log(containerMovements.innerHTML);
  console.log(`${value}`);
  });
  */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const move of movements) {
+  movementsUSDfor.push(move * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposit" : "withdraw"} ${mov}`,
+);
+console.log(movementsDescriptions);
+
 /////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
 // code challenge # 1
+// function checkDogs(dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   dogs.forEach((age, index) => {
+//     if (age >= 3) {
+//       console.log(
+//         `Dog number ${index + 1} is an adult, and is ${age} years old`,
+//       );
+//     } else {
+//       console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+//     }
+//   });
+// }
+//
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
