9ae77f92613452050945b361cd5926952cbb8355
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-22 02:29:52 -0800

    dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -377,3 +377,39 @@ btnSort.addEventListener('click', e => {
  console.log(11n / 3n);
  console.log(10 / 3);
  */
+/*
+ /////////////////////////////////////////////
+ // Creating dates
+ // Create a date
+ const now = new Date();
+ console.log(now);
+ 
+ console.log(new Date('Aug 02 2023 18:05:41'));
+ console.log(new Date('December 24, 2015'));
+ console.log(new Date(account1.movementsDates[0]));
+ 
+ console.log(new Date(2036, 10, 19, 15, 23, 5));
+ console.log(new Date(2036, 10, 33, 15, 23, 5));
+ 
+ console.log(new Date(0));
+ console.log(new Date(3 * 24 * 60 * 60 * 1000));
+ 
+ // working with dates
+ const future = new Date(2037, 10, 19, 15, 23);
+ console.log(future);
+ console.log(future.getFullYear());
+ console.log(future.getMonth());
+ console.log(future.getDate());
+ console.log(future.getDay());
+ console.log(future.getHours());
+ console.log(future.getMinutes());
+ console.log(future.getSeconds());
+ console.log(future.toISOString());
+ console.log(future.getTime());
+ console.log(new Date(2142285780000));
+ 
+ console.log(Date.now());
+ 
+ future.setFullYear(2040);
+ console.log(future);
+ */
