8d6e92977f93cae5d3a620a7756418ba3adff164
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-24 15:51:14 -0800

    constructor functions and the new operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function Person(firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  // never create methods in constructor functions
+}
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to Prototype
+// 4. function automatically return {}
+
+const lhzdnb = new Person('lhzdnb', 2002);
+const chiaki = new Person('chiaki', 2002);
+console.log(lhzdnb, chiaki);
+const lh = 'lh';
+console.log(lhzdnb instanceof Person);
+console.log(lh instanceof Person);
