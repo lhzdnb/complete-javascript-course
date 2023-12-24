ee273c60255a2b7931b02a703189eeeef055ba8b
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-24 15:54:54 -0800

    constructor functions and the new operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -11,6 +11,22 @@ function Person(firstName, birthYear) {
 // 3. {} linked to Prototype
 // 4. function automatically return {}
 
+/**
+ * The comments in the code explain what happens when the `new` keyword is
+ * used:
+ *
+ * 1. A new empty object `{}` is created.
+ * 2. The constructor function (`Person` in this case) is called with `this`
+ * set to the newly created object.
+ * 3. The new object is linked to the prototype of the constructor function.
+ * 4. The function automatically returns the new object.
+ *
+ * The `instanceof` operator is used to check if an object is an instance of a
+ * particular constructor function. In the code, it's used to check if `lhzdnb`
+ * and `lh` are instances of `Person`.
+ * @type {Person}
+ */
+
 const lhzdnb = new Person('lhzdnb', 2002);
 const chiaki = new Person('chiaki', 2002);
 console.log(lhzdnb, chiaki);
