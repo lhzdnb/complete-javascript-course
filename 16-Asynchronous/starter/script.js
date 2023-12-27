435d0997ac3ef21f7c77a4c58f21da2452c657c9
lh20020727 <lh20020727@sjtu.edu.cn>
2023-12-27 00:36:47 -0800

    building a simple promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -114,6 +114,7 @@ function getCountryData(country) {
     `Country: ${country} not found.`,
   )
     .then(data => {
+      console.log(data);
       renderCountry(data[0]);
       // Country 2
       if (!data[0]?.borders) throw new Error('No neighbour found!');
@@ -134,5 +135,116 @@ function getCountryData(country) {
 }
 
 btn.addEventListener('click', () => {
-  getCountryData('taiwan');
+  getCountryData('usa');
 });
+
+//////////////////////////////////////////////////
+// Coding Challenge #1
+/*
+ function whereAmI(lat, lng) {
+ fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+ .then(response => {
+ if (!response.ok)
+ throw new Error(`Problem with geocoding ${response.status}`);
+ return response.json();
+ })
+ .then(data => {
+ console.log(data);
+ const { city, country } = data;
+ if (city === 'Throttled! See geocode.xyz/pricing')
+ throw new Error(
+ 'You can send at most 3 requests per second. Try again later!',
+ );
+ console.log(`You are in ${city}, ${country}`);
+ return fetch(`https://restcountries.com/v3.1/name/${country}`);
+ })
+ .then(response => {
+ if (!response.ok)
+ throw new Error(`Country not found (${response.status}`);
+ return response.json();
+ })
+ .then(data => renderCountry(data[0]))
+ .catch(err => {
+ console.error(`Something went wrong: ${err.message}.`);
+ })
+ .finally(() => (countriesContainer.style.opacity = '1'));
+ }
+ 
+ const coords = [
+ [52.508, 13.381],
+ [19.037, 72.873],
+ [-33.933, 18.474],
+ ];
+ 
+ if (navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(
+ position => {
+ console.log(position);
+ whereAmI(position.coords.latitude, position.coords.longitude);
+ },
+ () => {
+ alert('Could not get your location');
+ },
+ );
+ }
+ setTimeout(() => whereAmI(...coords[0]), 1500);
+ setTimeout(() => whereAmI(...coords[1]), 3000);
+ setTimeout(() => whereAmI(...coords[2]), 4500);
+ */
+
+/*
+ console.log('Test start');
+ setTimeout(() => console.log('0 sec timer'), 0);
+ Promise.resolve('Resolved promise 1').then(res => console.log(res));
+ Promise.resolve('Resolved promise 2').then(res => {
+ for (let i = 0; i < 1000000000; i++) {}
+ console.log(res);
+ });
+ console.log('Test end');
+ */
+
+// Test start
+// Test end
+// resolved promise 1
+// 0 sec timer
+
+const lotteryPromise = new Promise((resolve, reject) => {
+  console.log('Lottery draw is happening');
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve('You win');
+    } else {
+      reject(new Error('You lose'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+function wait(seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+wait(2)
+  .then(() => {
+    console.log('1 sec');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 sec');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 sec');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('4 sec');
+    return wait(1);
+  });
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject('abc').catch(err => console.error(err));
